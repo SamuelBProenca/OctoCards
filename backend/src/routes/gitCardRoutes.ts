b99bfc2127c2735785378c8329918b197cf20544
samuel <samuelbrito782@gmail.com>
2025-02-26 00:56:52 -0300

    fix: correção na importação do gitCardController

--- backend/src/routes/gitCardRoutes.ts
+++ backend/src/routes/gitCardRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
-const { getGitCard } = require('../controllers/gitCardController');
+import { getGitCard } from '../controllers/gitCardController';
 
-// Define a rota para buscar informações do repositório
 const router = Router();
 router.get('/:owner/:repo', getGitCard);
 
